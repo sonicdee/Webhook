c3d66281a9e4b85dd91bf3462bd3348f2255c1b5
sonicdee <linus.hechinger@outlook.de>
2019-03-10 21:18:22 +0100

    Update actors.py
    added bytedata for on an off check

--- actors.py
+++ actors.py
@@ -15,6 +15,12 @@ bus = smbus.SMBus(1)
 DEVICE_ADDRESS = 0x20 #7 bit address (will be left shifted to add the read write
 DEVICE_REG_MODE1 = 0x06
 DEVICE_REG_DATA = 0xff
+mainpump_on = [248,250,252,254]
+mainpump_off = [249,251,253,255]
+heatpump_on = [249,252,253]
+heatpump_off = [251,254,255]
+light_on = [249,250,251]
+light_off = [253,254,255]
 
 def set_mainpump(onoff):
     global bus
@@ -31,10 +37,13 @@ def set_mainpump(onoff):
         bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
 
 def is_mainpump():
+    global mainpump_on
+    global mainpump_off
+
     bytedata = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
-    if bytedata == 254:
+    if bytedata in mainpump_on:
         onoff = True
-    if bytedata == 255:
+    if bytedata in mainpump_off:
         onoff = False
     return onoff
 
@@ -53,12 +62,15 @@ def set_heatpump(onoff):
         bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
 
 def is_heatpump():
-    # bytedata = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
-    # if bytedata == 254:
-    #     onoff = True
-    # if bytedata == 255:
-    #     onoff = False
-    return True
+    global heatpump_on
+    global heatpump_off
+    
+    bytedata = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
+    if bytedata in heatpump_on:
+        onoff = True
+    if bytedata in heatpump_off:
+        onoff = False
+    return onoff
 
 def set_light(onoff):
     global bus
@@ -75,9 +87,12 @@ def set_light(onoff):
         bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
 
 def is_light():
-    # bytedata = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
-    # if bytedata == 254:
-    #     onoff = True
-    # if bytedata == 255:
-    #     onoff = False
-    return True
+    global light_on
+    global light_off
+    
+    bytedata = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
+    if bytedata in light_on:
+        onoff = True
+    if bytedata in light_off:
+        onoff = False
+    return onoff
